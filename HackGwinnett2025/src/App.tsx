360f4f2624d86e27eb27953fe01ac97ee00df624
StephenA360 <144952935+StephenA360@users.noreply.github.com>
2025-10-25 13:15:38 -0400

    Updated App.tsx

--- HackGwinnett2025/src/App.tsx
+++ HackGwinnett2025/src/App.tsx
@@ -1,15 +1,16 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import {StepList} from "./Components/stepList.tsx"
-
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <StepList steps={}/>
+      <StepList steps={[]}/>
+      <div className="flex-row" id="footer">
+        <button>View Recipes</button>
+        <button>Create Recipe</button>
+      </div>
     </>
   )
 }
